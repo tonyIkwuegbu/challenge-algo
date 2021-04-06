b3a1f6ad599675aeaee286ecea73f7623a256d7b
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2021-04-06 13:21:29 +0100

    Update diagonalDifference.js

--- Interview Solution/diagonalDifference.js
+++ Interview Solution/diagonalDifference.js
@@ -1,9 +1,9 @@
 /*Given a square matrix, calculate the absolute difference 
 between the sums of its diagonals.*/
 
-  var arr =  [11, 2, 4]
-             [4, 5, 6 ]
-             [10, 8, -12]
+  var arr =     11, 2, 4
+                4, 5, 6 
+                10, 8, -12
 
 
 function diagonalDifference(arr) {
